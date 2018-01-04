7bf9137012f5f9c1006482bcc523a15955b1d0f3
wuxie2015 <luoyelin1989@126.com>
2018-01-04 20:20:13 +0800

    more strong

--- server/socket_server.py
+++ server/socket_server.py
@@ -12,7 +12,7 @@ host = '0.0.0.0'
 port = 12306
 ADDR = (host, port)
 
-
+BUFFER_SIZE = 4*1024
 
 
 class MyRequestHandler(socketserver.BaseRequestHandler):
@@ -28,7 +28,7 @@ class MyRequestHandler(socketserver.BaseRequestHandler):
         return md5
 
     def handle(self):
-        self.mq_obj = producer.mq_producer()
+        mq_obj = producer.mq_producer()
         print('connected from:', self.client_address)
         while True:
             # 定义文件信息。128s表示文件名为128bytes长，l表示一个int或log文件类型，在此为文件大小
@@ -36,7 +36,7 @@ class MyRequestHandler(socketserver.BaseRequestHandler):
             self.buf = self.request.recv(fileinfo_size)
             if self.buf:  # 如果不加这个if，第一个文件传输完成后会自动走到下一句
                 try:
-                    self.filename, self.filenamesize, self.filesize,self.md5 = struct.unpack(
+                    filename, filenamesize, filesize,md5 = struct.unpack(
                         '128sII32s', self.buf)  # 根据128sII32s解包文件信息，与client端的打包规则相同
                 except struct.error as e:
                     print(e)
@@ -44,40 +44,44 @@ class MyRequestHandler(socketserver.BaseRequestHandler):
                 try:
                     self.filesize = int(self.filesize)
                     # 文件名长度为128，大于文件名实际长度
-                    self.filename = self.filename[:self.filenamesize]
-                    self.md5 = self.md5.decode('utf8')
-                    print('filesize is: ', self.filesize, 'filename size is: ', len(self.filename))
-                    self.filenewname = os.path.join(
-                    '/usr/local/project/tmp_video/', ('new_' + self.filename.decode('utf8')).strip('\00').strip('\\x00'))  # 使用strip()删除打包时附加的多余空字符
+                    filename = filename[:filenamesize]
+                    md5 = md5.decode('utf8')
+                    print('notified that file name %s file size %s file md5 %s'%(filename,filesize,md5))
+                    filenewname = os.path.join(
+                    '/usr/local/project/tmp_video/', ('new_' + filename.decode('utf8')).strip('\00').strip('\\x00'))  # 使用strip()删除打包时附加的多余空字符
                 except Exception as e:
                     print(e)
-                    print("error in %s"%self.filename)
-                    recvd_size = 0
+                    print("error in %s"%filename)
                     while True:#清空缓冲区
-                        buf = self.request.recv(1024)
+                        buf = self.request.recv(BUFFER_SIZE)
                         if not len(buf):
                             break
                     continue#出现错误宁愿丢弃文件也不能影响程序运行
-                # self.mq_obj.put_message(self.filenewname)
-                print("ready to receive %s"%self.filenewname)
-                recvd_size = 0  # 定义接收了的文件大小
-                file = open(self.filenewname, 'wb')
-                print('start receiving...')
-                while not recvd_size == self.filesize:
-                    if self.filesize - recvd_size > 1024:
-                        rdata = self.request.recv(1024)
-                        recvd_size += len(rdata)
+                try:
+                    print("ready to receive %s"%filenewname)
+                    recvd_size = 0  # 定义接收了的文件大小
+                    file = open(filenewname, 'wb')
+                    print('start receiving...')
+                    while not recvd_size == self.filesize:
+                        if self.filesize - recvd_size > BUFFER_SIZE:
+                            rdata = self.request.recv(BUFFER_SIZE)
+                            recvd_size += len(rdata)
+                        else:
+                            rdata = self.request.recv(BUFFER_SIZE)
+                            # rdata = self.request.recv(filesize - recvd_size)
+                            recvd_size = self.filesize
+                        file.write(rdata)
+                    file.close()
+                    md5_recv = self.calc_md5(filenewname)
+                    if md5_recv == md5:
+                        print('receive done')
+                        mq_obj.put_message(filenewname)
                     else:
-                        rdata = self.request.recv(self.filesize - recvd_size)
-                        recvd_size = self.filesize
-                    file.write(rdata)
-                file.close()
-                md5_recv = self.calc_md5(self.filenewname)
-                if md5_recv == self.md5:
-                    print('receive done')
-                    self.mq_obj.put_message(self.filenewname)
-                else:
-                    print('md5 do not match')
+                        print('md5 do not match')
+                except Exception as e:
+                    print(e)
+                    print("file %s recieve failed"%filenewname)
+                    continue
         # self.request.close()
 
 
