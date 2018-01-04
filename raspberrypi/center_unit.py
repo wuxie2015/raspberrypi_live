09c4e8c3438c77b592e3b2ea1f784114ff848ecd
wuxie2015 <luoyelin1989@126.com>
2018-01-04 23:24:32 +0800

    socket will be close by server

--- raspberrypi/center_unit.py
+++ raspberrypi/center_unit.py
@@ -5,7 +5,7 @@ import time
 import os
 from socket_client import SocketClient
 from setting import HOST,PORT
-import multiprocessing
+import subprocess
 
 con = threading.Condition()
 
@@ -101,12 +101,10 @@ class PushStream(threading.Thread):
             else:
                 while not self.queue.empty():
                     file_path = self.queue.get()
-                    # self.socket_client.sendf(file_path)
-                    p = multiprocessing.Process(target= self.socket_client.sendf,args=(file_path,))
-                    p.start()
-                    p.join()
+                    subprocess.call("python socket_client.py -f %s"%file_path)
                     try:
                         os.system('rm -rf %s'%file_path)
+                        print('rm -rf %s'%file_path)
                     except Exception as e:
                         print(e)
                         try:
