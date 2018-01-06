d032568453db76e57d24cc872d4c476862c5df9b
wuxie2015 <luoyelin1989@126.com>
2018-01-07 01:20:09 +0800

    fix shell script

--- raspberrypi/raspberrypi_video.sh
+++ raspberrypi/raspberrypi_video.sh
@@ -1,38 +1,42 @@
 #! /bin/sh
+BASE_DIR = "/usr/local/project/raspberrypi_video/raspberrypi/alone/"
 CAPTURE_VIDEO = "/usr/local/project/raspberrypi_video/raspberrypi/alone/capture_video_alone.py"
 SOCKET_CLIENT = "/usr/local/project/raspberrypi_video/raspberrypi/alone/socket_client_alone.py"
 
 start() {
+    cd $BASE_DIR
     [ -x $SOCKET_CLIENT ] || exit 5
-    daemon $SOCKET_CLIENT
+    nohup python $SOCKET_CLIENT > /dev/null 2>&1 &
     retval1=$?
     [ -x $CAPTURE_VIDEO ] || exit 5
-    daemon $CAPTURE_VIDEO
+    nohup python $CAPTURE_VIDEO > /dev/null 2>&1 &
     retval2=$?
     $retval = `expr $retval1 + $retval2`
     return $retval
 }
 start_socket_client() {
+    cd $BASE_DIR
     [ -x $SOCKET_CLIENT ] || exit 5
-    daemon $SOCKET_CLIENT
+    nohup python $SOCKET_CLIENT > /dev/null 2>&1 &
     retval=$?
     return $retval
 }
 start_capture_video() {
+    cd $BASE_DIR
     [ -x $CAPTURE_VIDEO ] || exit 5
-    daemon $CAPTURE_VIDEO
+    nohup python $CAPTURE_VIDEO > /dev/null 2>&1 &
     retval=$?
     return $retval
 }
 stop() {
-    PID1=$(ps -e|grep capture_video_alone.py|awk '{printf $1}')
+    PID1=$(ps -e|grep capture_video_alone.py|grep ^[^grep]|awk '{printf $1}')
     kill -9 ${PID1}
     if [ $? -eq 0 ];then
     echo "kill $input1 success"
     else
         echo "kill $input1 fail"
     fi
-    PID2=$(ps -e|grep socket_client_alone.py|awk '{printf $1}')
+    PID2=$(ps -e|grep socket_client_alone.py|grep ^[^grep]|awk '{printf $1}')
     kill -9 ${PID2}
     if [ $? -eq 0 ];then
     echo "kill $input1 success"
@@ -46,28 +50,27 @@ restart() {
     start
 }
 watch_dog(){
-    p_count_capture_video_alone=$(ps -e|grep capture_video_alone.py|wc -l)
-    if [$p_count_capture_video -eq 0];then
+    p_count_socket_client_cmd=$(ps -e|grep socket_client_alone.py|grep ^[^grep]|wc -l)
+    p_count_socket_client=${p_count_socket_client_cmd}
+    if [[ $p_count_socket_client -eq 0 ]];then
         start_socket_client
     fi
-    p_count_socket_client_alone=$(ps -e|grep socket_client_alone.py|wc -l)
-    if [$p_count_scan_file -eq 0];then
+    p_count_capture_video_cmd=$(ps -e|grep capture_video_alone.py|grep ^[^grep]|wc -l)
+    p_count_capture_video=${p_count_capture_video_cmd}
+    if [[ $p_count_capture_video -eq 0 ]];then
         start_capture_video
     fi
 }
 
 case "$1" in
     start)
-        rh_status_q && exit 0
-        $1
+        start
         ;;
     stop)
-        rh_status_q || exit 0
-        $1
+        stop
         ;;
     watchdog)
         watch_dog || exit 0
-        $1
         ;;
     *)
         echo $"Usage: $0 {start|stop|watchdog}"
