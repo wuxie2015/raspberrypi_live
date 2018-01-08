68085aeeea586cca0a33626ece09a35732841733
wuxie2015 <luoyelin1989@126.com>
2018-01-08 14:34:12 +0800

    ffmpeg to localhost

--- server/scan_file.py
+++ server/scan_file.py
@@ -12,7 +12,7 @@ def scan_queue_file(ch, method, properties, body):
     f_name = body
     file_path = str(f_name.decode('utf8'))
     print(file_path)
-    subprocess.call("ffmpeg -re -i %s -vcodec libx264 -acodec copy -f flv -y rtmp://118.126.65.199:1935/live/livestream;" % file_path,
+    subprocess.call("ffmpeg -re -i %s -vcodec libx264 -acodec copy -f flv -y rtmp://127.0.0.1:1935/live/livestream;" % file_path,
                     stdout=subprocess.PIPE,shell=True)
     shutil.move(file_path, str(file_path) + '.bak')
 
