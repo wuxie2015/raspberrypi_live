16a5ee077e993027fcdba50bfc8d4cc42f979559
wuxie2015 <luoyelin1989@126.com>
2018-01-09 01:31:00 +0800

    change some parameter into env

---
+++ raspberrypi/python__rtmp/python_push_rtmp.py
@@ -0,0 +1,147 @@
+# -- coding: utf-8 --
+# http://blog.csdn.net/luhanglei
+import picamera
+import time
+import traceback
+import ctypes
+from librtmp import *
+
+global meta_packet
+global start_time
+
+
+class Writer():  # camera可以通过一个类文件的对象来输出，实现write方法即可
+    conn = None  # rtmp连接
+    sps = None  # 记录sps帧，发过以后就不需要再发了（抓包看到ffmpeg是这样的）
+    pps = None  # 同上
+    sps_len = 0  # 同上
+    pps_len = 0  # 同上
+
+    time_stamp = 0
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def write(self, data):
+        try:
+            # 寻找h264帧间隔符
+            indexs = []
+            index = 0
+            data_len = len(data)
+            while index < data_len - 3:
+                if ord(data[index]) == 0x00 and ord(data[index + 1]) == 0x00 and ord(
+                        data[index + 2]) == 0x00 and ord(data[index + 3]) == 0x01:
+                    indexs.append(index)
+                    index = index + 3
+                index = index + 1
+                # 寻找h264帧间隔符 完成
+            # 通过间隔符个数确定类型，树莓派摄像头的第一帧是sps+pps同时发的
+            if len(indexs) == 1:  # 非sps pps帧
+                buf = data[4: len(data)]  # 裁掉原来的头（00 00 00 01），把帧内容拿出来
+                buf_len = len(buf)
+                type = ord(buf[0]) & 0x1f
+                if type == 0x05:  # 关键帧，根据wire shark抓包结果，需要拼装sps pps 帧内容 三部分，长度都用4个字节表示
+                    body0 = 0x17
+                    data_body_array = [bytes(bytearray(
+                        [body0, 0x01, 0x00, 0x00, 0x00, (self.sps_len >> 24) & 0xff, (self.sps_len >> 16) & 0xff,
+                         (self.sps_len >> 8) & 0xff,
+                         self.sps_len & 0xff])), self.sps,
+                        bytes(bytearray(
+                            [(self.pps_len >> 24) & 0xff, (self.pps_len >> 16) & 0xff, (self.pps_len >> 8) & 0xff,
+                             self.pps_len & 0xff])),
+                        self.pps,
+                        bytes(bytearray(
+                            [(buf_len >> 24) & 0xff, (buf_len >> 16) & 0xff, (buf_len >> 8) & 0xff, (buf_len) & 0xff])),
+                        buf
+                    ]
+                    mbody = ''.join(data_body_array)
+                    time_stamp = 0  # 第一次发出的时候，发时间戳0，此后发真时间戳
+                    if self.time_stamp != 0:
+                        time_stamp = int((time.time() - start_time) * 1000)
+                    packet_body = RTMPPacket(type=PACKET_TYPE_VIDEO, format=PACKET_SIZE_LARGE, channel=0x06,
+                                             timestamp=time_stamp, body=mbody)
+                    packet_body.packet.m_nInfoField2 = 1
+                else:  # 非关键帧
+                    body0 = 0x27
+                    data_body_array = [bytes(bytearray(
+                        [body0, 0x01, 0x00, 0x00, 0x00, (buf_len >> 24) & 0xff, (buf_len >> 16) & 0xff,
+                         (buf_len >> 8) & 0xff,
+                         (buf_len) & 0xff])), buf]
+                    mbody = ''.join(data_body_array)
+                    # if (self.time_stamp == 0):
+                    self.time_stamp = int((time.time() - start_time) * 1000)
+                    packet_body = RTMPPacket(type=PACKET_TYPE_VIDEO, format=PACKET_SIZE_MEDIUM, channel=0x06,
+                                             timestamp=self.time_stamp, body=mbody)
+                self.conn.send_packet(packet_body)
+            elif len(indexs) == 2:  # sps pps帧
+                if self.sps is not None:
+                    return
+                data_body_array = [bytes(bytearray([0x17, 0x00, 0x00, 0x00, 0x00, 0x01]))]
+                sps = data[indexs[0] + 4: indexs[1]]
+                sps_len = len(sps)
+                pps = data[indexs[1] + 4: len(data)]
+                pps_len = len(pps)
+                self.sps = sps
+                self.sps_len = sps_len
+                self.pps = pps
+                self.pps_len = pps_len
+                data_body_array.append(sps[1:4])
+                data_body_array.append(bytes(bytearray([0xff, 0xe1, (sps_len >> 8) & 0xff, sps_len & 0xff])))
+                data_body_array.append(sps)
+                data_body_array.append(bytes(bytearray([0x01, (pps_len >> 8) & 0xff, pps_len & 0xff])))
+                data_body_array.append(pps)
+                data_body = ''.join(data_body_array)
+                body_packet = RTMPPacket(type=PACKET_TYPE_VIDEO, format=PACKET_SIZE_LARGE, channel=0x06,
+                                         timestamp=0, body=data_body)
+                body_packet.packet.m_nInfoField2 = 1
+
+                self.conn.send_packet(meta_packet, queue=True)
+                self.conn.send_packet(body_packet, queue=True)
+        except Exception, e:
+            traceback.print_exc()
+
+    def flush(self):
+        pass
+
+
+def get_property_string(string):  # 返回两字节string长度及string
+    length = len(string)
+    return ''.join([chr((length >> 8) & 0xff), chr(length & 0xff), string])
+
+
+def get_meta_string(string):  # 按照meta packet要求格式返回bytes,带02前缀
+    return ''.join([chr(0x02), get_property_string(string)])
+
+
+def get_meta_double(db):
+    nums = [0x00]
+    fp = ctypes.pointer(ctypes.c_double(db))
+    cp = ctypes.cast(fp, ctypes.POINTER(ctypes.c_longlong))
+    for i in range(7, -1, -1):
+        nums.append((cp.contents.value >> (i * 8)) & 0xff)
+    return ''.join(bytes(bytearray(nums)))
+
+
+def get_meta_boolean(isTrue):
+    nums = [0x01]
+    if (isTrue):
+        nums.append(0x01)
+    else:
+        nums.append(0x00)
+    return ''.join(bytes(bytearray(nums)))
+
+
+
+
+
+
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+    camera.start_recording(Writer(conn), format='h264', resize=(640, 480), intra_period=25,
+                           quality=25)  # 开始录制，数据输出到Writer的对象里
+    while True:  # 永远不停止
+        time.sleep(60)
+    camera.stop_recording()
+    camera.stop_preview()
---
+++ raspberrypi/python__rtmp/test_camera.py
@@ -0,0 +1,107 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from python_push_rtmp import Writer
+import librtmp
+from picamera import PiCamera
+import time
+import os
+from rabbitmq_util import producer
+from setting import HOST
+import logging
+from logging.handlers import RotatingFileHandler
+from io import BytesIO
+
+class VideoCapture:
+    def __init__(self):
+        self.logger = self.logger_init()
+
+    def logger_init(self):
+        # 定义一个RotatingFileHandler，最多备份5个日志文件，每个日志文件最大10M
+        log_file = 'capture_video_alone.log'
+        Rthandler = RotatingFileHandler(log_file, maxBytes=10 * 1024 * 1024, backupCount=5)
+        Rthandler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('\n%(asctime)s   %(filename)s[line:%(lineno)d]   %(levelname)s\n%(message)s')
+        Rthandler.setFormatter(formatter)
+        logger = logging.getLogger()
+        logger.addHandler(Rthandler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+    def init_camera(self):
+        '''input: nothing
+        output: videocapture object'''
+        brightness = int(os.getenv('VIDEO_BRIGHTNESS',70))
+        width = int(os.getenv('VIDEO_WIDTH', 640))
+        hight = int(os.getenv('VIDEO_HIGHT',480))
+        rotation = int(os.getenv('VIDEO_ROTATION',180))
+        framerate = int(os.getenv('VIDEO_FRAMERATE',25))
+        print('brightness',brightness)
+        print('width',width)
+        print('hight',hight)
+        print('rotation',rotation)
+        print('framerate',framerate)
+        camera = PiCamera()
+        camera.resolution = (width, hight)
+        camera.rotation = int(rotation)
+        camera.framerate = framerate
+        camera.brightness = brightness
+        return camera
+
+    def gen_file_name(self):
+        cur_time = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        file_name = "output_%s.h264" % cur_time
+        file_path = os.path.join(os.getcwd(),file_name)
+        return file_path
+
+
+    def init_rtmp(self):
+        conn = librtmp.RTMP(
+            "rtmp://%s:1935/live/livestream"%HOST,  # 推流地址
+            live=True)
+        librtmp.RTMP_EnableWrite(conn.rtmp)
+        conn.connect()
+        # 拼装视频格式的数据包
+        meta_body_array = [librtmp.get_meta_string('@setDataFrame'), librtmp.get_meta_string('onMetaData'),
+                           bytes(bytearray([0x08, 0x00, 0x00, 0x00, 0x06])),
+                           # 两个字符串和ECMA array头，共计6个元素,注释掉了音频相关数据
+                           librtmp.get_property_string('width'), librtmp.get_meta_double(640.0),
+                           librtmp.get_property_string('height'), librtmp.get_meta_double(480.0),
+                           librtmp.get_property_string('videodatarate'), librtmp.get_meta_double(0.0),
+                           librtmp.get_property_string('framerate'), librtmp.get_meta_double(25.0),
+                           librtmp.get_property_string('videocodecid'), librtmp.get_meta_double(7.0),
+                           # get_property_string('audiodatarate'), get_meta_double(125.0),
+                           # get_property_string('audiosamplerate'), get_meta_double(44100.0),
+                           # get_property_string('audiosamplesize'), get_meta_double(16.0),
+                           # get_property_string('stereo'), get_meta_boolean(True),
+                           # get_property_string('audiocodecid'), get_meta_double(10.0),
+                           librtmp.get_property_string('encoder'), librtmp.get_meta_string('Lavf57.56.101'),
+                           bytes(bytearray([0x00, 0x00, 0x09]))
+                           ]
+        meta_body = ''.join(meta_body_array)
+        print meta_body.encode('hex')
+        meta_packet = librtmp.RTMPPacket(type=librtmp.PACKET_TYPE_INFO, format=librtmp.PACKET_SIZE_LARGE, channel=0x04,
+                                 timestamp=0, body=meta_body)
+        meta_packet.packet.m_nInfoField2 = 1  # 修改stream id
+        stream = conn.create_stream(writeable=True)
+        return conn
+
+    def captuer_video(self):
+        '''capture video
+        input: none
+        output: none'''
+        camera = self.init_camera()
+        start_time = time.time()
+        camera.start_preview()
+        time.sleep(2)
+        conn = self.init_rtmp()
+        camera.start_recording(Writer(conn), format='h264', intra_period=25,
+                               quality=25)  # 开始录制，数据输出到Writer的对象里
+        while True:
+            time.sleep(60)
+            camera.stop_recording()
+            camera.stop_preview()
+
+if __name__ == '__main__':
+    vc_obj = VideoCapture()
+    vc_obj.captuer_video()
+
