4dcc857b2084ccb58d3ea8ac32719f7ece7ceee0
wuxie2015 <luoyelin1989@126.com>
2018-06-14 22:51:58 +0800

    modify

--- server/monitor_receiver/temperature/models.py
+++ server/monitor_receiver/temperature/models.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import mongoengine
-
 from django.db import models
 import uuid
 
 # Create your models here.
 
 class temperatureRecorde(models.Model):
-    id = mongoengine.UUIDField('uuid',primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField('uuid',primary_key=True, default=uuid.uuid4)
     temperature = models.IntegerField('当前温度',null=True, default=0)
     createTime = models.DateTimeField('数据创建时间',
                                       auto_now_add=True, db_index=True, null=True)
---
+++ server/monitor_receiver/temperature/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from temperature import views as views_cmp_host
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+# existing serializer, viewset, router registrations code
+...
+
+# Create our schema's view w/ the get_schema_view() helper method. Pass in
+# the proper Renderers for swagger
+schema_view = get_schema_view(
+    title='Users API',
+    renderer_classes=[
+        OpenAPIRenderer,
+        SwaggerUIRenderer])
+
+
+urlpatterns = [
+    url(r'doc$', schema_view, name="docs"),
+    url(r'templist$', views_cmp_host.temperatureList.as_view()),
+    url(r'templist$/(?P<timestamp>[A-Za-z0-9\-]+)/?$', views_cmp_host.temperatureDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
